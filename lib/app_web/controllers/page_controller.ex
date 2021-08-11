c70f146d712b45c5107cca8e02689e956b5de623
Kenji MINOURA <info@kandj.org>
2021-08-11 15:26:20 +0900

    1st

--- lib/app_web/controllers/page_controller.ex
+++ lib/app_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule AppWeb.PageController do
   use AppWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    #    render(conn, "index.html")
+    len = 1024
+    str = :crypto.strong_rand_bytes(len) |> Base.encode64 |> binary_part(0, len)
+    text conn, str
   end
 end
